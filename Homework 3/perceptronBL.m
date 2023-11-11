c43b479e7342d80866c75a805493a1071fa58840
TIHONLOSIK0 <ryazantsevta@gmail.com>
2023-11-11 14:18:54 +0300

    Added the files from moodle to new Homework 3 folder

---
+++ Homework 3/perceptronBL.m
@@ -0,0 +1,57 @@
+%--------------------------------------------------------------------------
+% Submission of:
+% Name, Student-ID
+% Name, Student-ID
+% Name, Student-ID
+% Name, Student-ID
+%--------------------------------------------------------------------------
+
+function [ w, iter, exitflag ] = perceptronBL( X, w, eta, maxIter )
+
+% Input
+% -----
+%
+% X        ... Data points and class labels.
+%              [ x_11, x_12, s_1;
+%                x_21, x_22, s_2;
+%                x_31, x_32, s_3;
+%                ...              ]
+%
+% w        ... Initial weight vector.
+%              [ w_1; w_2 ]
+%
+% eta      ... Step size.
+%
+% maxIter  ... Maximum number of iterations.
+
+% Output
+% ------
+%
+% w        ... Final weight vector.
+%
+% iter     ... Number of iterations needed.
+%
+% exitflag ... Exit flag.
+%              0 = No solution found, maximum number of iterations reached.
+%              1 = Solution found.
+
+% Determine number of data points.
+n = size(X, 1);
+
+% Initialize iteration counter, exit condition and exit flag.
+iter     = 0;
+exit     = 0;
+exitflag = 0;
+
+% While exit condition not met... 
+while ((~exit) && (iter < maxIter))
+    % Increment iteration counter.
+    iter = iter + 1;
+    
+    % ---------------------------------
+    % Your implementation goes here...
+    % ---------------------------------
+    
+end
+
+end
---
+++ Homework 3/perceptronOutput.m
@@ -0,0 +1,28 @@
+%--------------------------------------------------------------------------
+% Submission of:
+% Name, Student-ID
+% Name, Student-ID
+% Name, Student-ID
+% Name, Student-ID
+%--------------------------------------------------------------------------
+
+function [ f ] = perceptronOutput( x, w )
+
+% Input
+% -----
+%
+% x ... Data point.
+%
+% w ... Weight vector.
+%       [ w_1; w_2 ]
+
+% Output
+% ------
+%
+% f ... Perceptron output characteristic.
+
+% ---------------------------------
+% Your implementation goes here...
+% ---------------------------------
+
+end
