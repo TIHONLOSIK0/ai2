a52456fabd8dc5ca1c83ad8917c5a2f844c27c95
TIHONLOSIK0 <ryazantsevta@gmail.com>
2023-11-23 02:32:53 +0300

    Completed task 1 Homework 4 as well as added code snippets for the Task 2

---
+++ Homework 4/maxMarg.m
@@ -0,0 +1,36 @@
+function [exitflag, w, d, margin, dists] = maxMarg( X, y )
+
+% Input
+% -----
+%
+% X        ... Data points and class labels.
+%              [ x_11, x_12;
+%                x_21, x_22;
+%                x_31, x_32;
+%                ...              ]
+%
+% y        ... Class labels.
+%              [ s_1; s_2; s_3; ... ]
+
+% Output
+% ------
+%
+% exitflag ... Exitflag of quadprog.
+%
+% w        ... Weight vector.
+%
+% d        ... Distance from the origin.
+%
+% margin   ... Margin.
+%
+% dists    ... Distances of each data point to the separating plane.
+
+% STUDENT 1: Name, student ID
+% STUDENT 2: Name, student ID
+% STUDENT 3: Name, student ID
+% STUDENT 4: Name, student ID
+
+% YOUR IMPLEMENTATION GOES HERE...
+
+
+end
