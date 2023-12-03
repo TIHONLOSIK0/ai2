7c87511596174fe91ab7bbd41cdaee1980e21bd1
TIHONLOSIK0 <ryazantsevta@gmail.com>
2023-12-03 21:03:55 +0300

    Added Homework 6 Task 2 Code snippets

---
+++ Homework 6/maxMarg06.m
@@ -0,0 +1,40 @@
+function [exitflag, w, d, margin, dists, alphas, sv] = maxMarg05( X, y )
+
+% Input
+% -----
+%
+% X        ... Data points and class labels.
+%              [ x_11, x_12;
+%                x_21, x_22;
+%                x_31, x_32;
+%                ...              ]
+%
+% y        ... Class labels.
+%              [ s_1; s_2; s_3; ... ]
+
+% Output
+% ------
+%
+% exitflag ... Exitflag of quadprog.
+%
+% w        ... Weight vector.
+%
+% d        ... Bias of Separating Plane.
+%
+% margin   ... Margin.
+%
+% dists    ... Distances of each data point to the separating plane.
+%
+% alphas   ... Lagrange multipliers.
+%
+% sv       ... Indices of support vectors.
+
+% STUDENT 1: Name, student ID
+% STUDENT 2: Name, student ID
+% STUDENT 3: Name, student ID
+% STUDENT 4: Name, student ID
+
+% YOUR IMPLEMENTATION GOES HERE...
+
+
+end
